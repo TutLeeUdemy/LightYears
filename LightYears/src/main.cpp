23394d40b279ef1ac96cbe556baa510ca0d30cf9
JT <cg.jingtianli@gmail.com>
2023-10-07 13:18:14 -0500

    fetched SFML

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+	sf::RenderWindow renderWindow{sf::VideoMode(100,100), "MyWindow"};
+
 	std::cout << "hello world!" << std::endl;
 }
