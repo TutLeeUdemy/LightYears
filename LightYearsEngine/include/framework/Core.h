7642fd6d7f81ccff00ce9a0963f77688fd7d6ea0
JT <cg.jingtianli@gmail.com>
2023-10-13 12:27:25 -0500

    81 Defining Common Data Types

--- LightYearsEngine/include/framework/Core.h
+++ LightYearsEngine/include/framework/Core.h
@@ -1,9 +1,27 @@
 #include <stdio.h>
-
+#include <memory>
+#include <map>
+#include <unordered_map>
 namespace ly
 {
+template<typename T>
+using unique = std::unique_ptr<T>;
+
+template<typename T>
+using shared = std::shared_ptr<T>;
+
+template<typename T>
+using weak = std::weak_ptr<T>;
+
+template<typename T>
+using List = std::vector<T>;
+
+template<typename keyType, typename valType, typename Pr = std::less<keyType>>
+using Map = std::map<keyType, valType, Pr>;
+
+template<typename keyType, typename valType, typename hasher = std::hash<keyType>>
+using Dictionary = std::unordered_map<keyType, valType, hasher>;
 
 //a macro
 #define LOG(M, ...) printf(M "\n", ##__VA_ARGS__)
-
 }
