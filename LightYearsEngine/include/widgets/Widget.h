e608b8f019fa94d03d37013c4fbe4af4793f66d5
JT <cg.jingtianli@gmail.com>
2023-11-03 20:17:16 -0500

    139 Create The Widget Base Class

---
+++ LightYearsEngine/include/widgets/Widget.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "framework/Object.h"
+
+namespace ly
+{
+	class Widget : public Object
+	{
+	public:
+		void NativeDraw(sf::RenderWindow& windowRef);
+		virtual bool HandleEvent(const sf::Event& event);
+		void SetWidgetLocation(const sf::Vector2f& newLocation);
+		void SetWidgetRotation(float newRotation);
+		sf::Vector2f GetWidgetLocation() const { return mWidgetTransform.getPosition(); }
+		float GetWidgetRotation() const { return mWidgetTransform.getRotation(); }
+
+		void SetVisiblity(bool newVisibility);
+		bool GetVisiblity() const { return mIsVisible; }
+	protected:
+		Widget();
+	private:
+		virtual void Draw(sf::RenderWindow& windowRef);
+		virtual void LocationUpdated(const sf::Vector2f& newLocation);
+		virtual void RotationUpdated(float newRotaion);
+		sf::Transformable mWidgetTransform;
+		bool mIsVisible;
+	};
+}
---
+++ LightYearsEngine/src/widgets/Widget.cpp
@@ -0,0 +1,54 @@
+#include "widgets/Widget.h"
+
+namespace ly
+{
+	void Widget::NativeDraw(sf::RenderWindow& windowRef)
+	{
+		if (mIsVisible)
+		{
+			Draw(windowRef);
+		}
+	}
+	bool Widget::HandleEvent(const sf::Event& event)
+	{
+		return false;
+	}
+
+	void Widget::SetWidgetLocation(const sf::Vector2f& newLocation)
+	{
+		mWidgetTransform.setPosition(newLocation);
+		LocationUpdated(newLocation);
+	}
+
+	void Widget::SetWidgetRotation(float newRotation)
+	{
+		mWidgetTransform.setRotation(newRotation);
+		RotationUpdated(newRotation);
+	}
+
+	void Widget::SetVisiblity(bool newVisibility)
+	{
+		mIsVisible = newVisibility;
+	}
+
+	Widget::Widget()
+		: mIsVisible{true},
+		mWidgetTransform{}
+	{
+
+	}
+
+	void Widget::Draw(sf::RenderWindow& windowRef)
+	{
+
+	}
+
+	void Widget::LocationUpdated(const sf::Vector2f& newLocation)
+	{
+
+	}
+	void Widget::RotationUpdated(float newRotaion)
+	{
+
+	}
+}
