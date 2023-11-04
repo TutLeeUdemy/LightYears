4b294c623a2a5b4147f8460fe9729a41a5100241
JT <cg.jingtianli@gmail.com>
2023-11-04 16:43:47 -0500

    155 Layout the Basic Facilities of the Chaos Stage

---
+++ LightYearsGame/include/Enemy/ChaosStage.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "gameplay/GameStage.h"
+#include "framework/TimerManager.h"
+
+namespace ly
+{
+	class ChaosStage : public GameStage
+	{
+	public:
+		ChaosStage(World* world);
+
+		virtual void StartStage() override;
+
+	private:
+		virtual void StageFinished() override;
+		void SpawnVanguard();
+		void SpawnTwinBlade();
+		void SpawnHexgon();
+		void SpawnUFO();
+
+		void IncreseDifficulity();
+		void StageDurationReached();
+
+		sf::Vector2f GetRandomSpawnLocationTop() const;
+		sf::Vector2f GetRandomSpawnLocationSide() const;
+
+		float mSpawnInterval;
+		float mMinSpawnInterval;
+
+		float mSpawnIntervalDecrement;
+		float mSpawnIntervalDecrmentInterval;
+
+		float stageDuration;
+
+		TimerHandle mDifficultTimerHandle;
+		TimerHandle mSpawnTimer;
+	};
+}
