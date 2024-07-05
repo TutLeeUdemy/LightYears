8713e0e8a0bc8dd54c7587c33125aa2f7b9b239e
Jingtian Li <jingtian@pop-os.attlocal.net>
2024-07-05 12:52:33 -0500

    linux configture shell script added to the configurations file, box2d unit test disabled

---
+++ configurations/installDependencies.sh
@@ -0,0 +1,12 @@
+sudo apt install -y \
+libx11-dev \
+libxi-dev \
+libxrandr-dev \
+libglu1-mesa-dev freeglut3-dev mesa-common-dev \
+libudev-dev \
+libfreetype6-dev \
+libopenal-dev \
+libvorbis-dev \
+libflac++-dev \
+libxinerama-dev \
+libxcursor-dev 
